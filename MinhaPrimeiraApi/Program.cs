d0b9c89ff09a82e69e171447ce6377bc27116655
EduardoDev0706 <eduardodev2026@gmail.com>
2025-05-29 04:15:33 -0300

    Adicionando um novo projeto

---
+++ MinhaPrimeiraApi/Program.cs
@@ -0,0 +1,66 @@
+using Microsoft.EntityFrameworkCore;
+using MinhaPrimeiraApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyOrigin() // Permite requisições de qualquer origem (para desenvolvimento)
+                  .AllowAnyMethod() // Permite qualquer método HTTP (GET, POST, PUT, DELETE)
+                  .AllowAnyHeader(); // Permite qualquer cabeçalho na requisição
+        });
+});
+
+// Configura o DbContext para usar SQLite com a string de conexão
+builder.Services.AddDbContext<ApiDbContext>(options => options.UseSqlite(connectionString));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors();
+
+app.MapControllers();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
